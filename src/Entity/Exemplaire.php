11d225d1f3d9051663b061d55fa7014fd18ebfbf
mohamedDVP <mohamed.mak@laposte.net>
2024-04-06 12:55:35 +0200

    add other entity for migration in database

--- src/Entity/Exemplaire.php
+++ src/Entity/Exemplaire.php
@@ -13,8 +13,56 @@ class Exemplaire
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Usure $usure = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Stock $stock = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Livre $livre = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUsure(): ?Usure
+    {
+        return $this->usure;
+    }
+
+    public function setUsure(?Usure $usure): static
+    {
+        $this->usure = $usure;
+
+        return $this;
+    }
+
+    public function getStock(): ?Stock
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?Stock $stock): static
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getLivre(): ?Livre
+    {
+        return $this->livre;
+    }
+
+    public function setLivre(?Livre $livre): static
+    {
+        $this->livre = $livre;
+
+        return $this;
     }
 }
